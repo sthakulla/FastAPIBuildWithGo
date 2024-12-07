63c398909337d649eb2c17b0c40327b7ea3ad1ba
Subham <39816945+sthakulla@users.noreply.github.com>
2024-12-06 18:10:28 -0800

    Using json iterator to encode json msgs over calls

--- fastAPI.go
+++ fastAPI.go
@@ -19,15 +19,34 @@
 // 	log.Fatal(http.ListenAndServe(":8080", r))
 // }
 
+// package main
+
+// import (
+// 	"fmt"
+// 	"net/http"
+// )
+
+// func helloHandler(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintln(w, "Hello World!")
+// }
+
+//	func main() {
+//		http.HandleFunc("/hello", helloHandler)
+//		http.ListenAndServe(":8080", nil)
+//	}
 package main
 
 import (
-	"fmt"
 	"net/http"
+
+	jsoniter "github.com/json-iterator/go"
 )
 
+var json = jsoniter.ConfigCompatibleWithStandardLibrary
+
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello World!")
+	response := map[string]string{"message": "Hello, World!"}
+	json.NewEncoder(w).Encode(response)
 }
 
 func main() {
