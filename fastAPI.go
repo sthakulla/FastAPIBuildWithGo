bcc03f167e64fa2e36b6d93b83223dee7e37b6e8
Subham <39816945+sthakulla@users.noreply.github.com>
2024-12-06 18:08:30 -0800

    Comparing with regular http pkg to route calls

--- fastAPI.go
+++ fastAPI.go
@@ -1,20 +1,36 @@
+// package main
+
+// import (
+// 	"log"
+// 	"net/http"
+
+// 	"github.com/gorilla/mux"
+// )
+
+// func main() {
+// 	r := mux.NewRouter()
+// 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+// 		w.Write([]byte("Starting Gorilla Mux usage!! \nGo to localhost:8080/hello for routing in action"))
+// 	}).Methods("GET")
+// 	r.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+// 		w.Write([]byte("Hello, World!"))
+// 	}).Methods("GET")
+
+// 	log.Fatal(http.ListenAndServe(":8080", r))
+// }
+
 package main
 
 import (
-	"log"
+	"fmt"
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
-func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Starting Gorilla Mux usage!! \nGo to localhost:8080/hello for routing in action"))
-	}).Methods("GET")
-	r.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, World!"))
-	}).Methods("GET")
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello World!")
+}
 
-	log.Fatal(http.ListenAndServe(":8080", r))
+func main() {
+	http.HandleFunc("/hello", helloHandler)
+	http.ListenAndServe(":8080", nil)
 }
