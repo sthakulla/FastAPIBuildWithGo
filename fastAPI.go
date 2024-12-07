572175ef02a36159f279bba022a03dc680dbf0e8
Subham <39816945+sthakulla@users.noreply.github.com>
2024-12-06 18:13:29 -0800

    Handling postgres db call with goLang

--- fastAPI.go
+++ fastAPI.go
@@ -34,19 +34,57 @@
 //		http.HandleFunc("/hello", helloHandler)
 //		http.ListenAndServe(":8080", nil)
 //	}
+// package main
+
+// import (
+// 	"net/http"
+
+// 	jsoniter "github.com/json-iterator/go"
+// )
+
+// var json = jsoniter.ConfigCompatibleWithStandardLibrary
+
+// func helloHandler(w http.ResponseWriter, r *http.Request) {
+// 	response := map[string]string{"message": "Hello, World!"}
+// 	json.NewEncoder(w).Encode(response)
+// }
+
+// func main() {
+// 	http.HandleFunc("/hello", helloHandler)
+// 	http.ListenAndServe(":8080", nil)
+// }
+
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
 	"net/http"
 
-	jsoniter "github.com/json-iterator/go"
+	_ "github.com/lib/pq"
 )
 
-var json = jsoniter.ConfigCompatibleWithStandardLibrary
+var db *sql.DB
+
+func init() {
+	var err error
+	db, err = sql.Open("postgres", "user=username dbname=mydb sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	db.SetMaxOpenConns(25)
+	db.SetMaxIdleConns(25)
+}
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	response := map[string]string{"message": "Hello, World!"}
-	json.NewEncoder(w).Encode(response)
+	var message string
+	err := db.QueryRow("SELECT 'Hello, World!'").Scan(&message)
+	if err != nil {
+		http.Error(w, "Database error", http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintln(w, message)
 }
 
 func main() {
